bb7e21aac00a5a1b3d49f2c8c71aeff2622f0982
cshaptx4869 <994774638@qq.com>
2024-05-02 18:34:29 +0800

    refactor: :recycle: 优化axios响应数据TS类型提示

--- src/api/menu/index.ts
+++ src/api/menu/index.ts
@@ -1,10 +1,11 @@
 import request from "@/utils/request";
+import type { RouteRecordRaw } from "vue-router";
 
 /**
  * 获取路由列表
  */
 export function listRoutes() {
-  return request({
+  return request<any, ResponseData<RouteRecordRaw[]>>({
     url: "/api/v1/menus/routes",
     method: "get",
   });
